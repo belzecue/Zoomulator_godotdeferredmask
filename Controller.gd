490c98f35900a3876ea2a7ac617b504aa20d1746
Kim Simmons <zoomulator@gmail.com>
2020-09-06 14:26:02 +0200

    Tweak values.

--- Controller.gd
+++ Controller.gd
@@ -6,4 +6,4 @@ func _process(_delta: float) -> void:
 	var move = Vector2();
 	move.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	move.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	var _v = move_and_slide(move * 100.0)
+	var _v = move_and_slide(move * 50.0)
